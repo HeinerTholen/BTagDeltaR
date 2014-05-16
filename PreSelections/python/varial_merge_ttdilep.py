3c5afdb96c67d1adb5af479715a0172c2a5a3737
Heiner Tholen <heiner.tholen@cern.ch>
2014-05-16 19:36:21 +0200

    debugging merging script

--- PreSelections/python/varial_merge_ttdilep.py
+++ PreSelections/python/varial_merge_ttdilep.py
@@ -8,14 +8,16 @@ import varial.main
 import varial.sample
 
 
+tmp_dataset = '_tmp_das_datasets.txt'
+tmp_files = '_tmp_das_files_%s.txt'
 dbs_command_datasets = 'das_client.py --query "' \
     'instance=prod/phys03 ' \
-    'dataset=/*/htholen-TTdil*/USER' \
-    '" | grep htholen > _tmp_das_datasets.txt'
+    'dataset=/*/*tholen-TTdil*/USER' \
+    '" | grep htholen > ' + tmp_dataset
 dbs_command_files = 'das_client.py --query "' \
     'instance=prod/phys03 ' \
     'file dataset=%s' \
-    '" --limit 10000 | grep htholen > _tmp_das_files_%s.txt'
+    '" --limit 10000 | grep htholen > ' + tmp_files
 output_dir = '/nfs/dust/cms/user/tholenhe/samples/TTdilep/'
 
 
@@ -23,15 +25,16 @@ def parse_sample_name(dataset):
     #e.g. /MuEG/htholen-TTdilepData_RunA-d4aa20010c5c4332ad27156a5b618077/USER
     #                               ^^^^
     if 'TTdilepAllMC' in dataset:
-        back = dataset.split['TTdilepAllMC_'][1]
+        back = dataset.split('TTdilepAllMC_')[1]
     else:
-        back = dataset.split['TTdilepData_'][1]
-    return back.split['-'][0]
+        back = dataset.split('TTdilepData_')[1]
+    return back.split('-')[0]
 
 
 def main():
     # query datasets
-    os.system(dbs_command_datasets)
+    if not os.path.exists(tmp_dataset):
+        os.system(dbs_command_datasets)
     with open('_tmp_das_datasets.txt') as f:
         datasets = f.readlines()
     datasets = dict((parse_sample_name(d), d) for d in datasets)
@@ -39,8 +42,9 @@ def main():
     # query files
     files = {}
     for sample, dataset in datasets.iteritems():
-        os.system(dbs_command_files % (dataset, sample))
-        with open('_tmp_das_files_%s.txt' % sample) as f:
+        if not os.path.exists(tmp_files % sample):
+            os.system(dbs_command_files % (dataset, sample))
+        with open(tmp_files % sample) as f:
             files[sample] = f.readlines()
 
     samples = {}
@@ -51,7 +55,7 @@ def main():
 
         # make samples
         for i, grp in enumerate(groups):
-            name = '%s_%i' % sample, i
+            name = '%s_%d' % (sample, i)
             samples[name] = varial.sample.Sample(
                 name=name,
                 is_data="TTdilepData" in grp[0],
@@ -67,3 +71,7 @@ def main():
         suppress_cmsRun_exec=True,
         cfg_main_import_path="BTagDeltaR.PreSelections.MergeTtdilep_cfg"
     )
+
+
+if __name__ == '__main__':
+    main()
