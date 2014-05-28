b09e4671ed98675e7cd709c8c1f1e87330b717a4
Heiner Tholen <heiner.tholen@cern.ch>
2014-05-28 19:39:25 +0200

    ana

--- Analysis/python/varial_analysis.py
+++ Analysis/python/varial_analysis.py
@@ -6,22 +6,27 @@ import varial.main
 import varial.tools
 
 import ttdilep_samples
+samples = ttdilep_samples.smp_emu_mc
 
-varial.tools.ToolChain(
+
+tc = varial.tools.ToolChain(
     "ttdilep_analysis",
     [
         varial.tools.FwliteProxy(),
-        varial.tools.FSStackPlotter()
+        varial.tools.FSStackPlotter(),
+        varial.tools.SimpleWebCreator(),
     ]
 )
 
 
-varial.main.main(
-    not_ask_execute=True,
-    #fwlite_use_mp=False,
-    samples=ttdilep_samples.smp_emu_mc,
-    fwlite_executable=os.path.join(
-        os.environ['CMSSW_BASE'],
-        'src/BTagDeltaR/Analysis/python/vertexDR_worker.py',
+if __name__ == '__main__':
+    varial.main.main(
+        samples=samples,
+        active_samples = list(s.name for s in samples),
+        rootfile_postfixes=['.root', '.png'],
+        fwlite_executable=os.path.join(
+            os.environ['CMSSW_BASE'],
+            'src/BTagDeltaR/Analysis/python/vertexDR_worker.py',
+        ),
+        toolchain=tc,
     )
-)
